cd2f2db5a2d942901bce3d058d3cc1bdb42379cb
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-01-06 00:39:57 +0100

    end of the exercise

--- Document App/DocumentTableViewController.swift
+++ Document App/DocumentTableViewController.swift
@@ -17,13 +17,14 @@ extension Int {
 }
 
 class DocumentTableViewController: UITableViewController, QLPreviewControllerDelegate, QLPreviewControllerDataSource, UIDocumentPickerDelegate {
-
+    
     struct DocumentFile {
         var title: String
         var size: Int
         var imageName: String? = nil
         var url: URL
         var type: String
+        var section: Int
         
         static var filesData = [DocumentFile]()
     }
@@ -41,23 +42,24 @@ class DocumentTableViewController: UITableViewController, QLPreviewControllerDel
             let items = try fm.contentsOfDirectory(atPath: path)
             
             for item in items {
-                    if item.hasPrefix("cat") {
-                        let url = URL(fileURLWithPath: path + "/" + item)
-                        let resourceValues = try url.resourceValues(forKeys: [.typeIdentifierKey, .nameKey, .fileSizeKey])
-
-                        // Create a new DocumentFile instance
-                        let documentFile = DocumentFile(
-                            title: resourceValues.name ?? item,
-                            size: resourceValues.fileSize ?? 0,
-                            imageName: item,
-                            url: url,
-                            type: resourceValues.typeIdentifier ?? "unknown"
-                        )
-
-                        // Append the new instance to filesData
-                        DocumentFile.filesData.append(documentFile)
-                    }
+                if item.hasPrefix("cat") {
+                    let url = URL(fileURLWithPath: path + "/" + item)
+                    let resourceValues = try url.resourceValues(forKeys: [.typeIdentifierKey, .nameKey, .fileSizeKey])
+                    
+                    // Create a new DocumentFile instance with the assigned section
+                    let documentFile = DocumentFile(
+                        title: resourceValues.name ?? item,
+                        size: resourceValues.fileSize ?? 0,
+                        imageName: item,
+                        url: url,
+                        type: resourceValues.typeIdentifier ?? "unknown",
+                        section: 1
+                    )
+                    
+                    // Append the new instance to filesData
+                    DocumentFile.filesData.append(documentFile)
                 }
+            }
         } catch {
             print("Error: \(error)")
         }
@@ -122,11 +124,12 @@ class DocumentTableViewController: UITableViewController, QLPreviewControllerDel
             let documentFile = DocumentFile(
                 title: resourceValues.name ?? url.lastPathComponent,
                 size: resourceValues.fileSize ?? 0,
-                imageName: nil,  // Vous pouvez mettre à jour ceci en fonction du type de fichier
+                imageName: url.lastPathComponent,
                 url: url,
-                type: resourceValues.typeIdentifier ?? "unknown"
+                type: resourceValues.typeIdentifier ?? "unknown",
+                section: 0
             )
-
+                        
             // Ajouter le nouveau fichier à filesData
             DocumentFile.filesData.append(documentFile)
 
@@ -141,27 +144,57 @@ class DocumentTableViewController: UITableViewController, QLPreviewControllerDel
 
     // Indique au Controller combien de sections il doit afficher
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+        // Retourne le nombre de sections que vous voulez afficher
+        return 2
     }
         
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return DocumentFile.filesData.count
+        // Retourne le nombre de lignes dans chaque section
+        if section == 0 {
+            // Section "Importés"
+            return DocumentFile.filesData.filter { $0.section == 0 }.count
+        } else {
+            // Section "Bundle"
+            return DocumentFile.filesData.filter { $0.section == 1 }.count
+        }
+    }
+    
+    // Ajoute des en-têtes de section personnalisés
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Importés"
+        } else {
+            return "Bundle"
+        }
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let file = DocumentFile.filesData[indexPath.row]
+        let file: DocumentFile
+        if indexPath.section == 0 {
+            file = DocumentFile.filesData.filter { $0.section == 0 }[indexPath.row]
+        } else {
+            file = DocumentFile.filesData.filter { $0.section == 1 }[indexPath.row]
+        }
         instantiateQLPreviewController(withUrl: file.url, imageName: file.imageName)
     }
+
     
     // Indique au Controller comment remplir la cellule avec les données
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "DocumentCell", for: indexPath)
 
-        cell.textLabel?.text = DocumentFile.filesData[indexPath.row].title
-        cell.detailTextLabel?.text = String(DocumentFile.filesData[indexPath.row].size.formattedSize())
-        
+        if indexPath.section == 0 {
+            // Configurez la cellule pour la section "Importés"
+            let importedFiles = DocumentFile.filesData.filter { $0.section == 0 }
+            cell.textLabel?.text = importedFiles[indexPath.row].title
+            cell.detailTextLabel?.text = String(importedFiles[indexPath.row].size.formattedSize())
+        } else {
+            // Configurez la cellule pour la section "Bundle"
+            let bundleFiles = DocumentFile.filesData.filter { $0.section == 1 }
+            cell.textLabel?.text = bundleFiles[indexPath.row].title
+            cell.detailTextLabel?.text = String(bundleFiles[indexPath.row].size.formattedSize())
+        }
+
         return cell
     }
     
@@ -170,14 +203,17 @@ class DocumentTableViewController: UITableViewController, QLPreviewControllerDel
         previewController.dataSource = self
         previewController.delegate = self
         
-        // Transférer le nom de l'image directement à la classe QLPreviewControllerDataSource
-        previewController.currentPreviewItemIndex = DocumentFile.filesData.firstIndex { $0.url == url } ?? 0
+        // Utiliser l'indexPath directement
+        if let indexPath = DocumentFile.filesData.firstIndex(where: { $0.url == url }) {
+            previewController.currentPreviewItemIndex = indexPath
+        }
         
         // Présenter le QLPreviewController à l'aide du navigationController
         if let navigationController = self.navigationController {
             navigationController.pushViewController(previewController, animated: true)
         }
     }
+
     
     // MARK: - QLPreviewControllerDataSource
 
